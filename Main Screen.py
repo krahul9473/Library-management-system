590f5a808091193184504423936be230fd236df8
Joseph Mohanty <43529511+Joy-Anurag-Mohanty@users.noreply.github.com>
2019-06-18 22:56:03 +0530

    Update Main Screen.py

--- Main Screen.py
+++ Main Screen.py
@@ -1,17 +1,39 @@
-#Code for the main screen
-print('''Welcome to the library. Choose from below\n
-      1.Student
-      2. Faculty
-      3. Library staff
-      4. Read Library Rules''')
-while True:
-    try:
-        choice=int(input())
-    except NameError, ValueError:
-        continue
-    if(choice in [1, 2, 3, 4]):
-        break;
+import pandas as pd
 
-    
+from Student import Student
+from Faculty import Faculty
+
+print("Welcome to the Library.")
+print("Please Choose from the following \n")
+print(" 1. Student")
+print(" 2. Faculty")
+print(" 3. Library Staff")
+print(" 4. Library Rules \n")
+
+while True:
+    try :
+        choice = int(input("Enter the index please \n"))
+
+    except ValueError:
+        print("Sorry, enter a valid response \n")
+
+    else :
+        if choice in ([1,2,3,4]):
+            break
+
+if choice == 1:
+
+    print("Enter your student roll number: \n")
+    r_num = input()
+
+    print("Enter your password : \n")
+    key = input()
+
+    csv_path = 'Student.csv'
+
+    df = pd.read_csv(csv_path)
+     
+    #still need to add further code for authentication
+    #also we can import tim module for dates
 
 
