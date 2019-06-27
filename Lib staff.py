8ebf8a12acce54802add0b5a9125df4335fbdd94
Nilay Bhatia <45491806+nilaybhatia@users.noreply.github.com>
2019-06-28 00:57:41 +0530

    Update Lib staff.py
    Skeleton of functions

--- Lib staff.py
+++ Lib staff.py
@@ -1,5 +1,14 @@
 #Code for library staff
-print ("Library admin password")
-password=input()
+class LibStaff(object):
+  def addNewBook(self):
+    print("Add name of book and id number")
+    
+  def withdrawBook(self):
+    
+  def viewSuggestions(self):
+    
+  def totalFine(self):
+    
+  
 
 
