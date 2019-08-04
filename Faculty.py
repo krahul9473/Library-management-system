e14792c26420fed9f1999b1f4473a90ec970a519
havanoor <53264419+havanoor@users.noreply.github.com>
2019-08-04 12:37:58 +0530

    Update Faculty.py

--- Faculty.py
+++ Faculty.py
@@ -13,5 +13,33 @@ class Faculty(User):
         3. Return a book
         4. Suggest a new book for department''')
         
+        
+    option=int(input("Enter choice"))
+        
+        
+        if(choice==1):
+            
+            
+            name=input("Enter book name to issue")
+            cursor.execute('SELECT * FROM books WHERE Name="{}"'.format(name))
+            a=cursor.fetchone()
+            if(a[0]==None):
+                print("No book in records")
+            elif(a[3]=='A'):
+                      
+                cursor.execute('UPDATE  books SET status="I" WHERE  Name="{}"'.format(name))
+                print("Book issued")
+         
+        elif(choice==2):
+            pass
+        
+        
+        elif(choice==3):
+            
+            name=input("Enter book name to return")
+            cursor.execute('UPDATE books SET status="A" WHERE Name="{}"'.format(name))
+            print("book returned") 
+        
+            
 
             
